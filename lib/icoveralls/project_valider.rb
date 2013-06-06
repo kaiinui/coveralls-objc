de0d3c8842620ffc3c0d6fe4bcefa30038ead994
YaGeek <yannick.heinrich@gmail.com>
2013-06-06 22:29:03 +0200

    Add project valider class

---
+++ lib/icoveralls/project_valider.rb
@@ -0,0 +1,21 @@
+
+module Icoveralls
+
+class ProjectValider
+
+  def self.validProject(project_path)
+
+    if not File.exist?(project_path)
+        puts "File does not exist"
+      return false
+    else
+
+
+    end
+
+  end
+  # To change this template use File | Settings | File Templates.
+end
+
+
+end

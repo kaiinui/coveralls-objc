559240095642a7aebdcc836f40dd027aacf84153
HEINRICH Yannick <yannick.heinrich@gmail.com>
2013-06-10 10:46:41 +0200

    Go on with integration

--- lib/icoveralls/coveralls_sender.rb
+++ lib/icoveralls/coveralls_sender.rb
@@ -21,19 +21,14 @@ module Icoveralls
 
     def commit
 
-       json_file = MultiJson.dump(@json_file_hash, :pretty => true)
-    #  File.open("/Users/yaya/file.json",'w+') do |f|
-     #   f.write json_file
-     # end
-
-      # response = RestClient.post 'https://coveralls.io/api/v1/jobs', File.read('/Users/yaya/file.json'), :content_type => 'text/plain;charset=UTF-8'
-      # response_hash = MultiJson.load(response.to_str)
+       response = RestClient.post 'https://coveralls.io/api/v1/jobs', :json_file => self.class.hash_to_file(@json_file_hash)
+       response_hash = MultiJson.load(response.to_str)
     end
 
     def self.hash_to_file(hash)
       file = nil
       Tempfile.open(['icoveralls-upload', 'json']) do |f|
-        f.write(MultiJson.dump hash)
+        f.write(MultiJson.dump hash, :pretty => true)
         file = f
       end
       File.new(file.path, 'rb')
